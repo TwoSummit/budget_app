915706d13c555ab5ffc44825cdb44ad77355ade3
Corey <csscorey@gmail.com>
2019-02-15 16:23:40 -0600

    Update app.js
    no more app.configure

--- app.js
+++ app.js
@@ -39,46 +39,42 @@ if (template_engine == 'dust') {
     app.engine('hbs', hbs.engine);
 }
 
-app.configure(function() {
-
-    if (template_engine == 'swig') {
-        app.set('view engine', 'swig');
-        app.set('view options', {
-            layout: false
-        });
-    }
-
-    app.set('template_engine', template_engine);
-    app.set('domain', domain);
-    app.set('port', process.env.PORT || 8080);
-    app.set('views', __dirname + '\\views');
-    app.set('view engine', template_engine);
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser('wigglybits'));
-    app.use(expressSession.session({
-        secret: 'whatever',
-        store: store
-    }));
-    app.use(expressSession.session());
-    app.use(app.router);
-    app.use(require('less-middleware')(__dirname + '/public'));
-    app.use(express.static(path.join(__dirname, 'public')));
-
-    //middleware
-    app.use(function(req, res, next) {
-        if (req.session.user) {
-            req.session.logged_in = true;
-        }
-        res.locals.message = req.flash();
-        res.locals.session = req.session;
-        res.locals.q = req.body;
-        res.locals.err = false;
-        next();
+if (template_engine == 'swig') {
+    app.set('view engine', 'swig');
+    app.set('view options', {
+        layout: false
     });
+}
 
+app.set('template_engine', template_engine);
+app.set('domain', domain);
+app.set('port', process.env.PORT || 8080);
+app.set('views', __dirname + '\\views');
+app.set('view engine', template_engine);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('wigglybits'));
+app.use(expressSession.session({
+    secret: 'whatever',
+    store: store
+}));
+app.use(expressSession.session());
+app.use(app.router);
+app.use(require('less-middleware')(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//middleware
+app.use(function(req, res, next) {
+    if (req.session.user) {
+        req.session.logged_in = true;
+    }
+    res.locals.message = req.flash();
+    res.locals.session = req.session;
+    res.locals.q = req.body;
+    res.locals.err = false;
+    next();
 });
 
 app.configure('development', function() {
