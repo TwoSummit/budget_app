1b5a5ffaac5d2d290ba78f0b84dc9e58d910536a
Corey <csscorey@gmail.com>
2019-02-15 22:20:47 -0600

    Add files via upload
    Migrating to express 4

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ var template_engine = 'hbs',
     domain = 'localhost';
 
 var express = require('express'),
-    expressSession = require('express-session'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
     engine = require('ejs-locals'),
     routes = require('./routes'),
     http = require('http'),
-    store = new expressSession.MemoryStore,
+    errorhandler = require('errorhandler'),
     path = require('path');
 
 var app = express();
@@ -51,17 +52,9 @@ app.set('domain', domain);
 app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '\\views');
 app.set('view engine', template_engine);
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('wigglybits'));
-app.use(expressSession.session({
-    secret: 'whatever',
-    store: store
-}));
-app.use(expressSession.session());
-app.use(app.router);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'whatever', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
 app.use(require('less-middleware')(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -70,16 +63,16 @@ app.use(function(req, res, next) {
     if (req.session.user) {
         req.session.logged_in = true;
     }
-    res.locals.message = req.flash();
     res.locals.session = req.session;
     res.locals.q = req.body;
     res.locals.err = false;
     next();
 });
 
-app.configure('development', function() {
-    app.use(express.errorHandler());
-});
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+    app.use(errorhandler());
+}
 
 app.locals.inspect = require('util').inspect;
 app.get('/', routes.index);
